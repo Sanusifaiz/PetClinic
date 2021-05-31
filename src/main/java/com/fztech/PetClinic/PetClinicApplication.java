754191c0a3e2bed1a0309644f91a96e3c8416981
Sanusi faiz <mohammedfaizsanusi@gmail.com>
2021-05-31 22:08:38 +0100

    new commit

---
+++ src/main/java/com/fztech/PetClinic/PetClinicApplication.java
@@ -0,0 +1,15 @@
+package com.fztech.PetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		run(PetClinicApplication.class, args);
+	}
+khlhhll
+}
