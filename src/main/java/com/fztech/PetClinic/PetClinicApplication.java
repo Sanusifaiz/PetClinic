678aaa3f9f66d85d5ac2f53cc327a5a024df625a
Sanusi faiz <mohammedfaizsanusi@gmail.com>
2021-06-01 15:17:12 +0100

    Setting up Models

--- src/main/java/com/fztech/PetClinic/PetClinicApplication.java
+++ src/main/java/com/fztech/PetClinic/PetClinicApplication.java
@@ -11,5 +11,5 @@ public class PetClinicApplication {
 	public static void main(String[] args) {
 		run(PetClinicApplication.class, args);
 	}
-khlhhll
+
 }
---
+++ src/main/java/com/fztech/PetClinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.fztech.PetClinic.model;
+
+public class Owner extends Person {
+}
